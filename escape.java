78a576dcbc434d6666243a4567c689c8d9d41294
3028Bikalpa <3028bikalpa@gmail.com>
2024-06-19 17:55:53 +0900

    new

---
+++ escape.java
@@ -0,0 +1,7 @@
+public class escape {
+    public static void main(String[] args){
+        String escape = "Dear Bikalpa\nI hope this email finds you well\nThanks!";
+        System.out.println(escape);
+    }
+    
+}
