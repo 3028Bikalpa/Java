2a9840aeb85ac4487afb3c53b201ab8ccfd4706c
3028Bikalpa <3028bikalpa@gmail.com>
2024-06-19 17:51:46 +0900

    Additional

---
+++ additional.java
@@ -0,0 +1,146 @@
+
+
+
+/*public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
+public class Main {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int d = a + b + c;
+        System.out.println(d);
+    }
+}
+public class Main {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int c = a*b;
+        System.out.println(c);
+    }
+}
+public class Main {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int c = b-a;
+        System.out.println(c);
+    }
+}
+
+public class Main {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int c = b/a;
+        System.out.println(c);
+    }
+}
+public class Main {
+    public static void main(String[] args){
+        int a = 9;
+        int b = 20;
+        int c = b%a;
+        System.out.println(c);
+    }
+}
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Taking input from the user");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number 1");
+        int a = sc.nextInt();
+        System.out.println("Enter number 2");
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println("The sum of these numbers is "+ sum);
+    }
+}
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("This program will ask you the marks of the CBSE courses and will print the percentage.");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What was your marks in subject 1");
+        int a = sc.nextInt();
+        System.out.println("What was your marks in subject 2");
+        int b = sc.nextInt();
+        System.out.println("What was your marks in subject 3");
+        int c = sc.nextInt();
+        System.out.println("What was your marks in subject 4");
+        int d = sc.nextInt();
+        System.out.println("What was your marks in subject 5");
+        int e = sc.nextInt();
+        int total = a + b + c + d + e;
+        System.out.println("The total number of marks obtained by the students is "+ total);
+        double percentage = total/5;
+        System.out.println("The percentage obtained by this student is "+percentage+"%");
+    }
+}
+
+// java program to fidn the sum of three numbers
+public class Main{
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Give the first number");
+        int a = sc.nextInt();
+        System.out.println("Give the second number");
+        int b = sc.nextInt();
+        System.out.println("Give the third number");
+        int c = sc.nextInt();
+        int d = a+b+c;
+        System.out.println("The sum of the given numbers is "+ d);
+    }
+}
+
+public class Main{
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = sc.nextLine();
+        System.out.println("Hi "+ name+ ", how are you doing?");
+    }
+}
+
+// To check if the entered number is a integer or not
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Enter anything that you want to check is an integer or not");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+    }
+}
+
+public class Main{
+    public static void main(String[] args){
+        int a = 5;
+        int b = a--;
+        System.out.println(b);
+    }
+}
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println(2|3);
+    }
+}
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println(4.8%1.1);
+    }
+}*/
+
+public class additional{
+    public static void main(String[] args) {
+        String name = "Bikalpa";
+        System.out.println(name.startsWith("Bik"));
+    }
+}
+
+
