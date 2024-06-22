27105790b48880cf655e704d2afc478154196d24
3028Bikalpa <3028bikalpa@gmail.com>
2024-06-22 14:49:41 +0900

    dayoftheweek

---
+++ dayofweek.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class dayofweek {
+    public static void main(String[] args) {
+        System.out.println("Enter the position of the day of the week you want to find");
+        Scanner sc = new Scanner (System.in);
+        int day = sc.nextInt();
+        switch (day) {
+            case 1:
+                System.out.println("It is Monday");
+                break;
+            case 2:
+                System.out.println("It is Tuesday");
+                break;
+            case 3:
+                System.out.println("It is Wednesday");
+                break;
+            case 4:
+                System.out.println("It is Thursday");
+                break;
+            case 5:
+                System.out.println("It is Friday");
+                break;
+            case 6:
+                System.out.println("It is Saturday");
+                break;
+            case 7:
+                System.out.println("It is Sunday");
+                break;
+            default:
+                System.out.println("Invalid entry");
+        }        
+    }
+}
