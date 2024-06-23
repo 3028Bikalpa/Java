d43d7d178496d32d46e8277189f5e6bbe1ab7e80
3028Bikalpa <3028bikalpa@gmail.com>
2024-06-23 17:32:34 +0900

    forloop

---
+++ forloop.java
@@ -0,0 +1,12 @@
+public class forloop {
+    public static void main(String[] args) {
+        System.out.println("List of first 100 natural numbers");
+        for (int i = 1; i < 101; i++) {
+            System.out.println(i);
+        }
+        System.out.println("To print first 10 odd numbers");
+        for (int i = 0; i < 10; i++) {
+            System.out.println((2*i)+1);
+        }
+    }
+}
