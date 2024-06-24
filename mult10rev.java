9b84c69b890a181b8e2c766597fa0e2b6d94815c
3028Bikalpa <3028bikalpa@gmail.com>
2024-06-24 22:05:05 +0900

    mult10rev

---
+++ mult10rev.java
@@ -0,0 +1,7 @@
+public class mult10rev {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 1; i--) {
+            System.out.printf("10 * %d = %d\n", i, 10*i);
+        }
+    }
+}
