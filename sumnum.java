e7bf5ecb4da80b7201ae786037eee265d14c8d90
3028Bikalpa <3028bikalpa@gmail.com>
2024-06-24 20:14:52 +0900

    sum100nat

---
+++ sumnum.java
@@ -0,0 +1,11 @@
+public class sumnum {
+    public static void main(String[] args) {
+        int i = 0;
+        int sum=0;
+        while(i<101){
+            sum = sum+i;
+            i++;
+        }
+        System.out.println("The sum of first 100 natural numbers is "+ sum);
+    }
+}
