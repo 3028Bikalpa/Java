3f759c5470e531ce5b67753407fb4d2005804ad0
3028Bikalpa <3028bikalpa@gmail.com>
2024-06-25 18:18:40 +0900

    arraybasic

---
+++ array.java
@@ -0,0 +1,11 @@
+public class array {
+    public static void main(String[] args) {
+        int [] marks = new int[5];
+        marks [0]= 100;
+        marks [1]= 10;
+        marks [2]= 70;
+        marks [3]= 80;
+        marks [4]= 90;
+        System.out.println(marks[4]);
+    }
+}
