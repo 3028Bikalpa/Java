98a8a4a52734a1731c0be9a0a5b2dd58a33d8699
3028Bikalpa <3028bikalpa@gmail.com>
2024-06-25 18:45:55 +0900

    arrayloopnewsynt

---
+++ arrayloopnewsynt.java
@@ -0,0 +1,9 @@
+public class arrayloopnewsynt {
+    public static void main(String[] args) {
+        int [] marks = {98, 34, 53, 23, 56};
+        System.out.println("The marks you got are: ");
+        for (int element: marks) {
+            System.out.println(element);
+        }
+    }
+}
