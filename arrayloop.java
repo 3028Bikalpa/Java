1eb58497a3551328829c3a570370ad6df0ff91be
3028Bikalpa <3028bikalpa@gmail.com>
2024-06-25 18:24:53 +0900

    looprint

---
+++ arrayloop.java
@@ -0,0 +1,9 @@
+public class arrayloop {
+    public static void main(String[] args) {
+        int [] marks = {98, 34, 53, 23, 56};
+        System.out.println("The marks you got are: ");
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+}
