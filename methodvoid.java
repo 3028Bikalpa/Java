93ede68fef88d2d7941d92b06df567dc9acbf91b
3028Bikalpa <3028bikalpa@gmail.com>
2024-06-29 22:47:04 +0900

    method void

--- methodvoid.java
+++ methodvoid.java
@@ -2,15 +2,16 @@
 import java.util.Scanner;
 
 public class methodvoid {
-    static void noreturn(int x, int y){
-        int z = x+y;
+    static void noreturn(int x, int y, int t){
+        int z = x+y+t;
         System.out.println("The sum of the numbers is "+z);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the 2 numbers whose sum you want: ");
+        System.out.println("Enter the 3 numbers whose sum you want: ");
         int a = sc.nextInt();
         int b = sc.nextInt();
-        noreturn(a, b);
+        int c = sc.nextInt();
+        noreturn(a, b, c);
     }
 }
