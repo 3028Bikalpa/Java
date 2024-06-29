bb677f5d9d5b17ca171d4c63439e172eeea94375
3028Bikalpa <3028bikalpa@gmail.com>
2024-06-29 22:49:29 +0900

    multisum

--- multisum.java
+++ multisum.java
@@ -2,8 +2,8 @@ public class multisum {
     public static void main(String[] args) {
         int product = 0;
         for (int i = 1; i <= 10; i++) {
-            product = product+(8*i);
+            product = product+(7*i);
         }
-        System.out.println("The sum of the numbers in the multiplication table of 8 is "+product);
+        System.out.println("The sum of the numbers in the multiplication table of 7 is "+product);
     }
 }
