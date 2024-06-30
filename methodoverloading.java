af1ee0f788e7e90d8b96baaf607e7133954a89f8
3028Bikalpa <3028bikalpa@gmail.com>
2024-06-30 19:15:34 +0900

    methodoverloading

--- methodoverloading.java
+++ methodoverloading.java
@@ -5,9 +5,15 @@ public class methodoverloading {
     static void sample(int a ){
         System.out.println(a);
     }
+    static int square(int a){
+        int y = a*a;
+        return y;
+    }
     public static void main(String[] args) {
         int x = 5;
         sample();
         sample(x);
+        int ans = square(x);
+        System.out.println("The square of the number is: "+ ans);
     }
 }
