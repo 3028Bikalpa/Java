2c7fa2ff830efd8fd5c75e4efcbb7d932b19987b
3028Bikalpa <3028bikalpa@gmail.com>
2024-07-15 00:02:54 +0900

    override

--- override.java
+++ override.java
@@ -4,7 +4,7 @@ class A{
     }
 }
 class B extends A{
-    // @Override Not necesary to write it, but better to write it
+    @Override //It is not necessary to write @Override, but writing it helps
     public void met1(){
         System.out.println("This is method 1 from the class B");
     }
