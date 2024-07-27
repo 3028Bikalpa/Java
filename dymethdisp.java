99d6ca7b0b85a63435fef5e2501c9c24df07bcf1
3028Bikalpa <3028bikalpa@gmail.com>
2024-07-27 15:58:04 +0800

    dymethdisp

---
+++ dymethdisp.java
@@ -0,0 +1,24 @@
+class Phone{
+    public void ShowTime(){
+        System.out.println("This function will display time");
+    }
+    public void on(){
+        System.out.println("This function is to turn the phone on");
+    }
+}
+class SmartPhone extends Phone{
+    public void music(){
+        System.out.println("This function will play the music");
+    }
+    public void on(){
+        System.out.println("This function is to turn on the SmartPhone");
+    }
+}
+public class dymethdisp {
+    public static void main(String[] args) {
+        Phone oppo = new SmartPhone();
+        oppo.ShowTime();
+        oppo.on();
+
+    }
+}
