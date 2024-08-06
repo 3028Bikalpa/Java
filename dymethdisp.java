d879564b424e8cc59f62e5c2330601b1a1a62217
3028Bikalpa <3028bikalpa@gmail.com>
2024-08-06 23:18:49 +0800

    dymethdisp

--- dymethdisp.java
+++ dymethdisp.java
@@ -19,6 +19,6 @@ public class dymethdisp {
         Phone oppo = new SmartPhone();
         oppo.ShowTime();
         oppo.on();
-        // here we cannot use oppo.music but can write others
+        // here we cannot use oppo.music
     }
 }
